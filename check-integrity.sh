eaddd6cf3c625886590ebedf09a9be4ed7434d88
Aleksei Richards <alekseirichards@yahoo.com>
2013-05-07 08:50:40 -0400

    Now works with the chrome extension code

--- check-integrity.sh
+++ check-integrity.sh
@@ -9,23 +9,28 @@ TARS="$(mktemp -d)"
 # Get the git repository
 git clone --quiet git://github.com/$THEREPO/$THEREPO.github.io.git $REPOS
 
-# remove stuff that doesn't get deployed by the web.
+# remove files from the repo download that don't get deployed by the web.
+# we are only intersted in what goes to the browser
 rm -rf $REPOS/.git
 rm -rf $REPOS/README.md
 rm -rf $REPOS/CNAME
+rm -rf $REPOS/js/background.js
+rm -rf $REPOS/manifest.json
+rm -rf $REPOS/images/icons
+rm -rf $REPOS/font
 
 # Get the website
 wget -r -q http://$THESITE -P $WEBSITE
 
-ls -la $WEBSITE/$THESITE
-ls -la $REPOS
+# We have to ignore fonts as I never know which will load
+rm -rf $WEBSITE/$THESITE/font
 
 # Perform the check
 
 if diff -r $WEBSITE/$THESITE $REPOS; then
   echo "$THESITE matches the github repository."
 else
-  echo "$WEBSITE does not match the github repo."
+  echo "$THESITE does not match the github repo."
 fi
 
 # Remove folders
